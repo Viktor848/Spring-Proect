f8d941cff1bf267232b2c6ae2d712ba884707461
Viktor848 <viktority848@gmail.com>
2022-03-28 21:53:55 +0300

    Now with button for adding new Pokemons

--- src/main/java/com/example/Proect/AppController.java
+++ src/main/java/com/example/Proect/AppController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 
 @Controller
@@ -13,7 +15,29 @@ public class AppController {
     private PokemonRepository pokemonRepository;
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<Pokemon> listPokemons = pokemonRepository.findAll();
+        model.addAttribute("listPokemons", listPokemons);
+        return "index";
+    }
+
+    @GetMapping("/addPokemon")
+    public String addPokemon(Model model){
+        model.addAttribute("pokemon", new Pokemon());
+        return "addPokemon";
+    }
+
+    @PostMapping("/successfully_added")
+    public String added(Pokemon pokemon) {
+        pokemon.setName(pokemon.getName());
+        pokemon.setElement(pokemon.getElement());
+        pokemon.setHealth(pokemon.getHealth());
+        pokemon.setDamage(pokemon.getDamage());
+        pokemon.setDefense(pokemon.getDefense());
+        pokemon.setSize(pokemon.getSize());
+
+        pokemonRepository.save(pokemon);
+
         return "index";
     }
 
