b10ddc6d6ddcb2a52ec8c799b925e29943924499
Viktor848 <viktority848@gmail.com>
2022-04-09 17:35:42 +0300

    Fixed files

--- src/main/java/com/example/Proect/Repositories/EnemyPokemonRepository.java
+++ src/main/java/com/example/Proect/Repositories/EnemyPokemonRepository.java
@@ -3,5 +3,5 @@ package com.example.Proect.Repositories;
 import com.example.Proect.Entities.EnemyPokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnemyPokemonRepository extends JpaRepository<EnemyPokemon, Integer> {
+public interface EnemyPokemonRepository extends JpaRepository<EnemyPokemon, Long> {
 }
